422b75c946edfb36b85d1c2d0c472e9871976b6a
Luca Borrione <luca@email.com>
2021-04-25 15:31:08 +0100

    feat: add navigation elements
    - Media Queries for mobile, pad portrait and pad landscape +
    - Jquery actions to handle the search field

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$('main.blog > .sidebar .button').click(() => {
+const navSearchButton = $('nav > .search.button');
+const navSearchIcon = $('nav > .search.button > .icon');
+const navSearchField = $('nav > div.ui.search');
+
+$('nav > .sidebar-menu.button').click(() => {
   $('.ui.sidebar').sidebar('toggle');
+});
+
+$('nav > .search.button').click(function () {
+  navSearchField.toggle('slide');
+  navSearchIcon.toggleClass('x').toggleClass('search');
+  navSearchButton.toggleClass('active');
+  navSearchButton.blur();
+});
+
+$(window).on('orientationchange', function () {
+  navSearchField.css('display', '');
+  navSearchIcon.removeClass('x active').addClass('search');
 });
